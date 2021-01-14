0df42207a4eb9d8e3084f0f5a1dccb0a613f96e2
softelevation <vsandhu@softelevation.com>
2021-01-14 18:55:28 +0530

    google map

--- src/app/views/theme/brokermap.component.ts
+++ src/app/views/theme/brokermap.component.ts
@@ -2,25 +2,55 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
+  styles: ['agm-map { height: 300px; /* height is required */ }'],
   templateUrl: 'brokermap.component.html'
 })
 export class BrokermapComponent implements OnInit {
 
-  name = 'Angular 5';
-  lat:any;
-  lng:any;
+  lat = 43.879078;
+  lng = -103.4615581;
+  selectedMarker;
+  markers = [
+    // These are all just random coordinates from https://www.random.org/geographic-coordinates/
+    { lat: 22.33159, lng: 105.63233, alpha: 1 },
+    { lat: 7.92658, lng: -12.05228, alpha: 1 },
+    { lat: 48.75606, lng: -118.859, alpha: 1 },
+    { lat: 5.19334, lng: -67.03352, alpha: 1 },
+    { lat: 12.09407, lng: 26.31618, alpha: 1 },
+    { lat: 47.92393, lng: 78.58339, alpha: 1 }
+  ];
 
-  constructor(){
-    if (navigator) {
-    navigator.geolocation.getCurrentPosition( pos => {
-        this.lng = +pos.coords.longitude;
-        this.lat = +pos.coords.latitude;
-      });
-    }
+  addMarker(lat: number, lng: number) {
+    this.markers.push({ lat, lng, alpha: 0.4 });
   }
 
-  ngOnInit(): void {
+  max(coordType: 'lat' | 'lng'): number {
+    return Math.max(...this.markers.map(marker => marker[coordType]));
   }
+
+  min(coordType: 'lat' | 'lng'): number {
+    return Math.min(...this.markers.map(marker => marker[coordType]));
+  }
+
+  selectMarker(event) {
+    this.selectedMarker = {
+      lat: event.latitude,
+      lng: event.longitude
+    };
+  }
+
+  // constructor(){
+  //   if (navigator){
+  //   navigator.geolocation.getCurrentPosition( pos => {
+  //       this.lng = +pos.coords.longitude;
+  //       this.lat = +pos.coords.latitude;
+  //     });
+  //   }
+  // }
+
+  // ngOnInit(): void {
+  // }
+
 
  
 
