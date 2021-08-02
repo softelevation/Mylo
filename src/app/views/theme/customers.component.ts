aa28b7bcee3ead119e76d8195736b55598065b93
vijay sandhu <vijay.pro2020@gmail.com>
2021-08-02 12:23:42 +0530

    fix code

--- src/app/views/theme/customers.component.ts
+++ src/app/views/theme/customers.component.ts
@@ -1,25 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { AppServiceService } from './../../app-service.service';
+import { Component, OnInit } from "@angular/core";
+import { AppServiceService } from "./../../app-service.service";
 
 @Component({
-  templateUrl: 'customers.component.html'
+  templateUrl: "customers.component.html",
 })
 export class CustomersComponent implements OnInit {
-
   users: getAllUsers;
   error: string;
-  constructor(
-    private appService: AppServiceService
-  ) { }
+  constructor(private appService: AppServiceService) {}
 
-  
   ngOnInit(): void {
     this.appService.getAllUsers().subscribe(
-      (data: getAllUsers) => this.users = data['data'],
-      error => this.error = error
+      (data: getAllUsers) => (this.users = data["data"]),
+      (error) => (this.error = error)
     );
   }
-
 }
 
 interface getAllUsers {
